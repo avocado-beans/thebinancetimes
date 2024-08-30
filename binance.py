b61ad69686a58415f66a19360122e2cfcb00c288
Estifanos Tolemariam <122219518+avocado-beans@users.noreply.github.com>
2024-08-30 08:31:48 +0200

    Add files via upload

---
+++ binance.py
@@ -0,0 +1,60 @@
+from telethon import TelegramClient, events
+from fastapi import FastAPI
+import threading
+import uvicorn
+import asyncio
+import time
+import cmc
+import os
+
+api_id=os.environ['ID']
+api_hash=os.environ['HASH']
+cmc_key=os.environ['CMC_KEY']
+
+announcement_source='t.me/BWEnews'
+record='t.me/thebinancetimes'
+
+app = FastAPI()
+@app.get("/")
+async def confirm():
+    return """My process is purely logistical, narrowly focused by design."""
+def mask():
+    uvicorn.run(app)
+
+async def IncomingMessageListener(event):
+    start = time.time()
+    message = event.message.message
+    print(message)
+    await client.send_message(entity=record,message=message)
+    if 'Binance' in message:
+        try:
+            symbol = message.split('(')[1].split(')')[0]
+            message = cmc.cmc_stats(symbol, cmc_key)
+            print(message)
+            await client.send_message(entity=record,message=message)
+            (f"Got token info in ~{time.time()-start} seconds")
+        except:
+            pass
+async def con():
+    await client.connect()
+    print("Connected")
+    try:
+        await client.run_until_disconnected()
+    except:
+        pass
+async def discon(timer):
+    await asyncio.sleep(timer)
+    print("Disconnected")
+    await client.disconnect()
+async def parallel(timer):
+    await asyncio.gather(con(), discon(timer))
+    print("Killed")
+
+url_mask = threading.Thread(target=mask)
+url_mask.start()
+
+while True:
+    client = TelegramClient('anon', api_id, api_hash,)
+    client.add_event_handler(IncomingMessageListener, events.NewMessage(chats=announcement_source,incoming=True))
+    with client:
+        client.loop.run_until_complete(parallel(600))
---
+++ cmc.py
@@ -0,0 +1,28 @@
+import requests
+
+cmc_key = 'dd608883-1a40-4292-8c05-b12e2541ae44'
+
+def cmc_stats(symbol, cmc_key) -> None:
+    response = requests.get(
+    url='https://pro-api.coinmarketcap.com/v2/cryptocurrency/info',
+    headers={
+        'Accepts': 'application/json',
+        'X-CMC_PRO_API_KEY': cmc_key,
+        },
+    params={
+        'symbol':symbol,
+        })
+    message = f"Didn't find anything on ({symbol})"
+    if 'data' in response.json():
+        message = f"Found {len(response.json()['data'][symbol])} projects that matches the symbol ({symbol})."
+        for data in response.json()['data'][symbol][:5]:
+            try:
+                chain = data['platform']['symbol']
+                token_address = data['platform']['token_address']
+                message += f"\n------------------------------\nChain: {chain}\nAddress: {token_address}"
+            except:
+                pass
+    return message
+
+if __name__ == "__main__":
+    symbol = cmc_stats(input("Enter symbol: ").upper(), cmc_key)
